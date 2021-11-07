2c3ec8425825d41545fa6e67c887d3e8ffca05ef
Facundo Berges <facundo.h.berges@gmail.com>
2021-11-06 21:29:32 -0300

    tarea clase 3

--- index.js
+++ index.js
@@ -1,63 +1,45 @@
-// Tarea clase 1 y 2
+// Tarea clase 3
 
-let nombre;
-let apellido;
-let ingresosBrutos;
-let categoria;
+let numeroMinimo;
+let numeroMaximo;
+let cantidadNrosPares = 0;
 
-//Ingreso del usuario
-nombre = prompt("Ingrese nombre: ");
-apellido = prompt("Ingrese apellido: ");
-ingresosBrutos = parseFloat(prompt("Ingrese facturacion mensual bruta: "));
+//Ingresos de datos del usuario
+numeroMinimo = parseInt(
+  prompt(
+    "Bienvenido al contador de numeros pares entre 2 numeros ingresados.\n\nPor favor, ingrese un numero (numero inicial): "
+  )
+);
 
-//Verifico que la facturacion sea un numero mayor a 0
-while (isNaN(ingresosBrutos) || ingresosBrutos < 0) {
-  ingresosBrutos = parseFloat(
-    prompt("Error. Ingrese facturacion mensual (con numeros mayores a 0): ")
+//Verifico que el dato ingresado sea un numero
+while (isNaN(numeroMinimo)) {
+  numeroMinimo = parseInt(
+    prompt("Error. ingrese un numero (numero inicial): ")
   );
 }
 
-console.log(nombre);
-console.log(apellido);
-console.log(ingresosBrutos);
+//Ingresos de datos del usuario
+numeroMaximo = parseInt(
+  prompt("Por favor, ingrese un numero (numero final): ")
+);
 
-if (ingresosBrutos > 3700000) {
-  categoria = "responsable inscripto";
-} else {
-  categoria = "monotributista categoria ";
-  if (ingresosBrutos > 3335000) {
-    categoria = categoria + "K";
-  } else if (ingresosBrutos > 2910000) {
-    categoria = categoria + "J";
-  } else if (ingresosBrutos > 2600000) {
-    categoria = categoria + "I";
-  } else if (ingresosBrutos > 2100000) {
-    categoria = categoria + "H";
-  } else if (ingresosBrutos > 1750000) {
-    categoria = categoria + "G";
-  } else if (ingresosBrutos > 1400000) {
-    categoria = categoria + "F";
-  } else if (ingresosBrutos > 1060000) {
-    categoria = categoria + "E";
-  } else if (ingresosBrutos > 770000) {
-    categoria = categoria + "D";
-  } else if (ingresosBrutos > 550000) {
-    categoria = categoria + "C";
-  } else if (ingresosBrutos > 370000) {
-    categoria = categoria + "B";
-  } else {
-    categoria = categoria + "A";
+//Verifico que el dato ingresado sea un numero
+while (isNaN(numeroMaximo)) {
+  numeroMaximo = parseInt(prompt("Error. ingrese un numero (numero final): "));
+}
+
+for (i = numeroMinimo; i <= numeroMaximo; i++) {
+  if (i % 2 == 0) {
+    cantidadNrosPares = cantidadNrosPares + 1;
   }
 }
 
 alert(
-  "Bienvenido " +
-    nombre +
-    " " +
-    apellido +
-    ". Su facturacion mensual fue de $ " +
-    ingresosBrutos +
-    ". Usted se deberia encontrar inscripto como " +
-    categoria +
-    "."
+  "Entre los numeros " +
+    numeroMinimo +
+    " y " +
+    numeroMaximo +
+    " hay un total de " +
+    cantidadNrosPares +
+    " numeros pares."
 );
